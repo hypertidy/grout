b96bf5a679c1d1d82e80c361a319ba9ca73252fa
Michael Sumner <mdsumner@gmail.com>
2021-08-25 10:37:43 +1000

    fix missing projection meta

---
+++ R/tiles.R
@@ -0,0 +1,164 @@
+
+# from a raster or matrix, tile out
+# the dimensions based on blockX/blockY
+.tilescheme <- function(x, blockX = 256L, blockY = 256L) {
+  dm <- as.integer(dim(x))[1:2]
+  blockX <- as.integer(blockX)
+  blockY <- as.integer(blockY)
+  if (dm[2L] <= blockX) {
+    ntilesX <- 1L
+  } else {
+    ntilesX <- (dm[2L] %/% blockX)
+    if (ntilesX * blockX < dm[2L]) ntilesX <- ntilesX + 1L  ## hack
+  }
+  if (blockX == 1) ntilesX <- dm[2L]
+  dangleX <- (ntilesX * blockX) - dm[2L] 
+  if (dm[1L] <= blockY) {
+    ntilesY <- 1L
+  } else {
+    ntilesY <- (dm[1L] %/% blockY)
+  }
+  if (ntilesY * blockY < dm[1L]) ntilesY <- ntilesY + 1  ## hack
+  if (blockY == 1) ntilesY <- dm[1L]
+  dangleY <- (ntilesY * blockY) - dm[1L] 
+  structure(list(inputraster = raster::raster(x), 
+       ntilesX = ntilesX, ntilesY = ntilesY, 
+       dangleX = dangleX, dangleY = dangleY, 
+       blockX = blockX, blockY = blockY), 
+       class = "grout_tilescheme")
+
+}
+
+extent.grout_tilescheme <- function(x) {
+  raster::extent(raster::xmin(x$inputraster), 
+                 raster::xmin(x$inputraster) +  (x$blockX *  x$ntilesX) * raster::res(x$inputraster)[1], 
+                 raster::ymax(x$inputraster) -  (x$blockY *  x$ntilesY) * raster::res(x$inputraster)[2], 
+                 raster::ymax(x$inputraster) )
+}
+
+#' Create a tiling scheme from a raster
+#'
+#' The input may be an actual raster or a matrix. There is an assumed
+#' block size of 256x256, and the scheme will record the number of tiles
+#' in each dimension and the amount of "overlapping dangle" when the dimensions
+#' of the data don't fit neatly within the tiles. 
+#' 
+#' The tile scheme object has print and plot methods for basic information. 
+#' 
+#' See function [as_polys()] to generate tiles from the scheme. 
+#' @param x a raster, or a matrix
+#' @param blockX tile dimensions in columns (X)
+#' @param blockY tile diemnsions in rows (Y)
+#'
+#' @return A "tile scheme" object with information about the tile spacing and extent. 
+#' @export
+#'
+#' @examples
+#' rv <- raster::raster(volcano, xmn = 0, xmx = nrow(volcano), 
+#'                               ymn = 0, ymx = ncol(volcano))
+#' ## one (too) big tile
+#' tile <- tiles(rv)
+#' plot(tile)
+#' tile
+#' 
+#' raster::image(rv, add = TRUE)
+#' axis(1); axis(2)
+#' 
+#' ## more size appropriate
+#' tils <- tiles(rv, 8, 8)
+#' plot(tils)
+#' raster::image(rv, add = TRUE)
+#' plot(tils, add = TRUE)
+tiles <- function(x, blockX = 256, blockY = 256) {
+  ts <- .tilescheme(x, blockX = blockX, blockY = blockY)
+  rt <- raster::raster(extent.grout_tilescheme(ts), 
+                       ncol = ts$ntilesX, nrow = ts$ntilesY, crs = raster::crs(x))
+  structure(list(tileraster = rt,
+                 scheme = ts),
+            class = "grout_tiles")
+}
+
+#' print tiles
+#' @param x a grout [tiles()] object
+#' @param ... ignored
+#' @export
+print.grout_tiles <- function(x, ...) { 
+  dm <- dim(x$tileraster)
+  ex <- raster::extent(x$tileraster)
+  cat(sprintf("          tiles: %i, %i (x * y = %i)\n",  dm[2L], dm[1L], prod(dm)) )
+  cat(sprintf("          block: %i, %i \n", x$scheme$blockX, x$scheme$blockY) )
+  cat(sprintf("         dangle: %i, %i \n", x$scheme$dangleX, x$scheme$dangleY))
+  rs <- raster::res(x$tileraster)
+  cat(sprintf("tile resolution: %s, %s \n", format(rs[1L]), format(rs[2L])))
+  cat(sprintf("    tile extent: %s, %s, %s, %s (xmin,xmax,ymin,ymax)\n", 
+              format(raster::xmin(ex)), 
+                     format(raster::xmax(ex)), 
+                            format(raster::ymin(ex)),
+                                   format(raster::ymax(ex))))
+  rg <- raster::res(x$scheme$inputraster)
+  cat(sprintf("          grain: %s, %s (%i : x, %i : y)", format(rg[1L]), format(rg[2L]), x$scheme$blockX, x$scheme$blockY))
+  invisible(NULL)
+}
+
+#' plot tiles
+#' 
+#' @param x a grout [tiles()] object
+#' @param ... arguments passed to methods (particularly [sp::plot()])
+#' @param border the colour of the tile border (default grey)
+#' @param lwd the width of the tile border (default = 2)
+#' @importFrom graphics plot
+#' @export
+plot.grout_tiles <- function(x, ..., border = "grey", lwd = 2) {
+  sp::plot(as_polys(x), ..., border = border, lwd = lwd)
+  raster::plot(raster::extent(x$scheme$inputraster), add = TRUE, col = "firebrick", lty = 2)
+  invisible(NULL)
+}
+
+#' Create rect (extent) table
+#' 
+#' Data frame of extents xmin, xmax, ymin, ymax. 
+#' 
+#' @param x  a grout [tiles()] object 
+#'
+#' @param ... ignored
+#'
+#' @name as_rect
+#' @return data frame of tile extents
+#' @export
+#' 
+as_rect <- function(x, ...) {
+  UseMethod("as_rect")
+}
+.p2s <- function(x) {
+  cbind(vx0 = x[-length(x)],
+  .vx1 = x[-1L])
+} 
+#' @name as_rect
+#' @export
+as_rect.grout_tiles <- function(x, ...) {
+  xx <- seq(raster::xmin(x$tileraster), raster::xmax(x$tileraster), length = ncol(x$tileraster) + 1L)
+  yy <- seq(raster::ymin(x$tileraster), raster::ymax(x$tileraster), length = nrow(x$tileraster) + 1L)
+ 
+  idx <- .p2s(seq_along(xx))
+  idy <- .p2s(seq_along(yy))
+  tibble::as_tibble(cbind(expand.grid(x0 = xx[idx[,1L]], y0 = yy[idy[,1L]]), 
+                 expand.grid(x1 = xx[idx[,2L]], y1 = yy[idy[,2L]])))
+}
+#' Tiles as polygons
+#' 
+#' @param x a grout [tiles()] object 
+#' @param ... ignored
+#'
+#' @export
+as_polys <- function(x, ...) {
+  UseMethod("as_polys")
+}
+#' @name as_polys
+#' @importFrom methods as
+#' @export
+as_polys.grout_tiles <- function(x, ...) {
+  pp <- as(x$tileraster, "SpatialPolygonsDataFrame")
+  pp$tile <- seq(raster::ncell(x$tileraster))
+  pp$layer <- NULL
+  pp
+}
