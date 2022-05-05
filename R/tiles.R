717a7bc9d3fe22ccd37ff07831742429d29b699a
Michael Sumner <mdsumner@gmail.com>
2022-05-05 21:02:45 +1000

    begin move from raster

--- R/tiles.R
+++ R/tiles.R
@@ -1,10 +1,10 @@
 
 # from a raster or matrix, tile out
 # the dimensions based on blockX/blockY
-.tilescheme <- function(x, blockX = 256L, blockY = 256L) {
-  dm <- as.integer(dim(x))[1:2]
-  blockX <- as.integer(blockX)
-  blockY <- as.integer(blockY)
+.tilescheme <- function(x, blocksize) {
+  dm <- as.integer(x$dimension)
+  blockX <- blocksize[1L]
+  blockY <- blocksize[2L]
   if (dm[2L] <= blockX) {
     ntilesX <- 1L
   } else {
@@ -21,7 +21,7 @@
   if (ntilesY * blockY < dm[1L]) ntilesY <- ntilesY + 1  ## hack
   if (blockY == 1) ntilesY <- dm[1L]
   dangleY <- (ntilesY * blockY) - dm[1L] 
-  structure(list(inputraster = raster::raster(x), 
+  structure(list(inputraster = x, 
        ntilesX = ntilesX, ntilesY = ntilesY, 
        dangleX = dangleX, dangleY = dangleY, 
        blockX = blockX, blockY = blockY), 
@@ -30,10 +30,12 @@
 }
 
 extent.grout_tilescheme <- function(x) {
-  raster::extent(raster::xmin(x$inputraster), 
-                 raster::xmin(x$inputraster) +  (x$blockX *  x$ntilesX) * raster::res(x$inputraster)[1], 
-                 raster::ymax(x$inputraster) -  (x$blockY *  x$ntilesY) * raster::res(x$inputraster)[2], 
-                 raster::ymax(x$inputraster) )
+                c(x$inputraster$extent[1L], 
+                 x$inputraster$extent[1L] +  (x$blockX *  x$ntilesX) * vaster:::x_res(x$inputraster$extent, 
+                                                                                      x$inputraster$dimension), 
+                 x$inputraster$extent[4L] -  (x$blockY *  x$ntilesY) * vaster:::y_res(x$inputraster$extent, 
+                                                                                      x$inputraster$dimension), 
+                 x$inputraster$extent[4L] )
 }
 
 #' Create a tiling scheme from a raster
@@ -46,7 +48,7 @@ extent.grout_tilescheme <- function(x) {
 #' The tile scheme object has print and plot methods for basic information. 
 #' 
 #' See function [as_polys()] to generate tiles from the scheme. 
-#' @param x a raster, or a matrix
+#' @param x a raster specification, a list with extent, dimension, projection
 #' @param blockX tile dimensions in columns (X)
 #' @param blockY tile diemnsions in rows (Y)
 #'
@@ -57,7 +59,7 @@ extent.grout_tilescheme <- function(x) {
 #' rv <- raster::raster(volcano, xmn = 0, xmx = nrow(volcano), 
 #'                               ymn = 0, ymx = ncol(volcano))
 #' ## one (too) big tile
-#' tile <- tiles(rv)
+#' tile <- grout(volcano, c(256L, 256L))
 #' plot(tile)
 #' tile
 #' 
@@ -69,39 +71,64 @@ extent.grout_tilescheme <- function(x) {
 #' plot(tils)
 #' raster::image(rv, add = TRUE)
 #' plot(tils, add = TRUE)
-tiles <- function(x, blockX = 256, blockY = 256) {
+grout <- function(x, blocksize = NULL) {
+  if (is.array(x)) {
+    x <- list(extent = c(0, nrow(x), 0, ncol(x)), 
+              dimension = dim(x)[1:2], 
+              projection = NA_character_)
+  }
   has_names <- function(x, nms) {
     all(nms %in% names(x))
   }
-  if (is.list(x) && has_names(x, c("dimXY", "extent", "projection"))) {
-    x <- raster::raster(raster::extent(x$extent), ncols = x$dimXY[1], nrows = x$dimXY[2], crs = x$projection)
+  edp <- c("extent", "dimension", "projection")
+  if (is.character(x)) {
+    x <- vapour::vapour_raster_info(x)
   }
-  ts <- .tilescheme(x, blockX = blockX, blockY = blockY)
-  rt <- raster::raster(extent.grout_tilescheme(ts), 
-                       ncol = ts$ntilesX, nrow = ts$ntilesY, crs = raster::crs(x))
+  if (is.list(x) && has_names(x, edp)) {
+    if (is.null(blocksize) && has_names(x, "tiles")) {
+        blocksize <- x$tiles
+    }
+  } else {
+    if (is.list(x) && has_names(x, c("extent", "dimXY", "projection"))) {
+      x <- setNames(x[c("extent", "dimXY", "projection")], edp)
+    }
+      
+  }
+  if (is.null(blocksize)) blocksize <- c(x$dimension[1L], 1L)
+  ts <- .tilescheme(x, blocksize)
+
+   # rt <- raster::raster(extent.grout_tilescheme(ts), 
+   #                     ncol = ts$ntilesX, nrow = ts$ntilesY, crs = raster::crs(x))
+  rt <-  list(extent = extent.grout_tilescheme(ts), dimension = c(ts$ntilesX, ts$ntilesY), projection = x$projection)
   structure(list(tileraster = rt,
                  scheme = ts),
             class = "grout_tiles")
 }
 
+tiles <- function(... )  {
+  .Deprecated("grout")
+  grout(...)
+}
 #' print tiles
 #' @param x a grout [tiles()] object
 #' @param ... ignored
 #' @export
 print.grout_tiles <- function(x, ...) { 
-  dm <- dim(x$tileraster)
-  ex <- raster::extent(x$tileraster)
+  dm <- x$tileraster$dimension
+  ex <- x$tileraster$extent
   cat(sprintf("          tiles: %i, %i (x * y = %i)\n",  dm[2L], dm[1L], prod(dm)) )
   cat(sprintf("          block: %i, %i \n", x$scheme$blockX, x$scheme$blockY) )
   cat(sprintf("         dangle: %i, %i \n", x$scheme$dangleX, x$scheme$dangleY))
-  rs <- raster::res(x$tileraster)
+  rs <- c(vaster:::x_res(ex, dm), vaster:::y_res(ex, dm))
   cat(sprintf("tile resolution: %s, %s \n", format(rs[1L]), format(rs[2L])))
   cat(sprintf("    tile extent: %s, %s, %s, %s (xmin,xmax,ymin,ymax)\n", 
-              format(raster::xmin(ex)), 
-                     format(raster::xmax(ex)), 
-                            format(raster::ymin(ex)),
-                                   format(raster::ymax(ex))))
-  rg <- raster::res(x$scheme$inputraster)
+              format(ex[1L]), 
+                     format(ex[2L]), 
+                            format(ex[3L]),
+                                   format(ex[4L])))
+  
+  rg <- c(vaster:::x_res(ex, dm), 
+          vaster:::y_res(ex, dm))
   cat(sprintf("          grain: %s, %s (%i : x, %i : y)", format(rg[1L]), format(rg[2L]), x$scheme$blockX, x$scheme$blockY))
   invisible(NULL)
 }
@@ -159,12 +186,46 @@ as_rect.grout_tiles <- function(x, ...) {
 as_polys <- function(x, ...) {
   UseMethod("as_polys")
 }
+x_corner <- function(x) {
+  xl <- x$extent[1:2]
+  ##resx <- vaster:::x_res(x$extent, x$dimension)
+  seq(xl[1L], xl[2L], length.out = x$dimension[1L] + 1L)
+}
+y_corner <- function(x) {
+  yl <- x$extent[3:4]
+  ##resy <- vaster:::y_res(x$tileraster$extent, x$tileraster$dimension)
+  seq(yl[1L], yl[2L], length.out = x$dimension[2L] + 1L)
+}
+col_from_cell <- 
+function(x, cell) {
+  cell <- round(cell)
+  cell[cell < 1L | cell > prod(x$dimension)] <- NA
+  rownr <- trunc((cell - 1)/x$dimension[2L]) + 1L
+  as.integer(cell - ((rownr - 1) * x$dimension[1L]))
+}
+row_from_cell <- 
+function(x, cell) {
+  cell <- round(cell)
+  cell[cell < 1 | cell > prod(x$dimension)] <- NA
+  trunc((cell - 1)/x$dimension[1L]) + 1
+}
+
 #' @name as_polys
 #' @importFrom methods as
 #' @export
 as_polys.grout_tiles <- function(x, ...) {
-  pp <- as(x$tileraster, "SpatialPolygonsDataFrame")
-  pp$tile <- seq(raster::ncell(x$tileraster))
-  pp$layer <- NULL
+   stop("not working atm")
+  ## WIP
+  xs <- x_corner(x$tileraster)
+  ys <- y_corner(x$tileraster)
+  cell <- seq_len(prod(x$dimension))
+  idx <- col_from_cell(x$tileraster, cell)
+  jdx <- row_from_cell(x$tileraster, cell)
+  
+  grd <- expand.grid(idx, jdx)
+  grd <- cbind(grd, grd + 1)
+  # pp <- as(x$tileraster, "SpatialPolygonsDataFrame")
+  # pp$tile <- seq(raster::ncell(x$tileraster))
+  # pp$layer <- NULL
   pp
 }
